8316774af197547704a744929d9456c2c8c6d62b
p3terson7 <psarateanu@hotmail.ca>
2024-02-21 11:13:43 -0500

    Java SpringBoot + PostgreSQL project created

---
+++ masterchief/src/main/java/com/example/masterchief/MasterchiefApplication.java
@@ -0,0 +1,13 @@
+package com.example.masterchief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterchiefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterchiefApplication.class, args);
+	}
+
+}
---
+++ masterchief/src/test/java/com/example/masterchief/MasterchiefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masterchief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterchiefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
