79b66230a3e68a2a91afca2fce2a102dcff623d4
p3terson7 <psarateanu@hotmail.ca>
2024-02-23 12:42:02 -0500

    Model: classe Client

---
+++ masterchief/masterchief-backend/src/main/java/com/example/masterchief/model/Client.java
@@ -0,0 +1,34 @@
+package com.example.masterchief.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ToString(callSuper = true)
+@Entity
+@Data
+@NoArgsConstructor
+public class Client extends User{
+    private String address;
+    private String phone;
+
+    public Client(
+            Long id,
+            String firstname,
+            String lastname,
+            String email,
+            String password,
+            String address,
+            String phone
+    ) {
+        super(id, firstname, lastname, email, password);
+        this.address = address;
+        this.phone = phone;
+    }
+}
