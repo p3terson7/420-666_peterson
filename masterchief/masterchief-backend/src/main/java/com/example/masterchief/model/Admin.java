b8ade7db04812c3a5cc1335e4387ceff2ef227e5
p3terson7 <psarateanu@hotmail.ca>
2024-02-23 12:46:35 -0500

    Model: List<Conversation> conversations dans Admin

--- masterchief/masterchief-backend/src/main/java/com/example/masterchief/model/Admin.java
+++ masterchief/masterchief-backend/src/main/java/com/example/masterchief/model/Admin.java
@@ -15,6 +15,9 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class Admin extends User{
+
+    @OneToMany(mappedBy = "admin", cascade = CascadeType.ALL)
+    protected List<Conversation> conversations = new ArrayList<>();
     public Admin(
             Long id,
             String firstname,
