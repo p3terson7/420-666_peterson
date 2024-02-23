720f6574221fba869939e58a9ac5ae8ecae3d966
p3terson7 <psarateanu@hotmail.ca>
2024-02-23 12:38:51 -0500

    Model : classe User

---
+++ masterchief/masterchief-backend/src/main/java/com/example/masterchief/model/User.java
@@ -0,0 +1,27 @@
+package com.example.masterchief.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQUENCE_USER")
+    @SequenceGenerator(name = "SEQUENCE_USER", sequenceName = "USER_SEC", allocationSize = 1)
+    protected Long id;
+    protected String firstName;
+    protected String lastName;
+    @Column(unique = true)
+    protected String email;
+    @ToString.Exclude
+    protected String password;
+
+//    public abstract UserDTO toDTO();
+}
