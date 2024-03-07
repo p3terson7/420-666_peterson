ea70c94cba747d72a6b23d006802fa448c8e62f3
p3terson7 <psarateanu@hotmail.ca>
2024-03-07 16:29:18 -0500

    signup : http constant

---
+++ masterchief/masterchief-frontend/src/constants/https.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosResponse } from "axios";
+
+const http = axios.create({
+    baseURL: "https://localhost:8080",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export default http;
