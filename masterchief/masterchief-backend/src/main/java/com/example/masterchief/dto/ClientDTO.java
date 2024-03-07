d5befb3efd1c9aa42742679694eaedaec51384f1
p3terson7 <psarateanu@hotmail.ca>
2024-03-07 15:24:48 -0500

    couche_DTO : ClientDTO

--- masterchief/masterchief-backend/src/main/java/com/example/masterchief/dto/ClientDTO.java
+++ masterchief/masterchief-backend/src/main/java/com/example/masterchief/dto/ClientDTO.java
@@ -1,2 +1,39 @@
-package com.example.masterchief.dto;public class ClientDTO {
+package com.example.masterchief.dto;
+
+import com.example.masterchief.model.Client;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientDTO extends UserDTO {
+    private String address;
+    private String phone;
+
+    public ClientDTO(Long id,
+                     String firstName,
+                     String lastName,
+                     String email,
+                     String password,
+                     String address,
+                     String phone)
+    {
+        super(id, firstName, lastName, email, password);
+        this.address = address;
+        this.phone = phone;
+    }
+    @Override
+    public Client fromDTO() {
+        return new Client(
+                id,
+                firstName,
+                lastName,
+                email,
+                password,
+                address,
+                phone
+        );
+    }
 }
