71a125d62cf4979ef926e2e2330022b05835f2b2
p3terson7 <psarateanu@hotmail.ca>
2024-03-14 19:59:57 -0400

    signup: generic form updated

--- masterchief/masterchief-frontend/src/components/GenericForm/index.tsx
+++ masterchief/masterchief-frontend/src/components/GenericForm/index.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React, {useEffect, useRef, useState} from 'react';
+import {Button, Container, Form} from 'react-bootstrap';
 import { GenericField } from './GenericField';
+import "../../App.css";
+import Card from "react-bootstrap/Card";
 
 interface FieldConfig {
     label: string;
@@ -20,11 +22,9 @@ export const GenericForm: React.FC<GenericFormProps> = ({ steps, onSubmit }) => 
     const [currentStep, setCurrentStep] = useState(0);
     const [formData, setFormData] = useState<Record<string, string>>({});
     const [errors, setErrors] = useState<Record<string, string>>({});
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+    const activeStepRef = useRef<HTMLDivElement>(null);
+    const [containerHeight, setContainerHeight] = useState('auto');
+    const [attemptedNext, setAttemptedNext] = useState(false);
 
     const validateCurrentStep = () => {
         const currentFields = steps[currentStep];
@@ -32,64 +32,111 @@ export const GenericForm: React.FC<GenericFormProps> = ({ steps, onSubmit }) => 
         let isValid = true;
 
         currentFields.forEach(field => {
-            if (field.validationRule && !field.validationRule(formData[field.name], formData)) {
+            console.log("Field validation : ", field.validationRule, field.validationRule!(formData[field.name], formData));
+            const value = formData[field.name];
+            if (field.validationRule && !field.validationRule(value, formData)) {
                 newErrors[field.name] = field.errorMessage || 'Invalid field';
                 isValid = false;
             }
         });
 
         setErrors(newErrors);
+        console.log("Errors: ", errors);
         return isValid;
     };
 
-    const handleNext = () => {
-        if (validateCurrentStep()) {
+    useEffect(() => {
+        if (attemptedNext && validateCurrentStep()) {
+            setCurrentStep(currentStep + 1);
+            setAttemptedNext(false);
+        } else {
+            setAttemptedNext(false);
+        }
+
+    }, [attemptedNext, currentStep, formData]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+        console.log(formData);
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (currentStep === steps.length - 1) {
+            await onSubmit(formData);
             setCurrentStep(currentStep + 1);
         }
+    };
+
+    const handleNext = () => {
+        setAttemptedNext(true);
     };
 
     const handlePrev = () => {
         setCurrentStep(currentStep - 1);
     };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (validateCurrentStep()) {
-            await onSubmit(formData);
-        }
-    };
-
     return (
-        <Form onSubmit={handleSubmit}>
-            {steps[currentStep].map((field) => (
-                <GenericField
-                    key={field.name}
-                    label={field.label}
-                    type={field.type}
-                    name={field.name}
-                    value={formData[field.name] || ''}
-                    placeholder={field.placeholder}
-                    onChange={handleChange}
-                    isInvalid={!!errors[field.name]}
-                    errorMessage={errors[field.name]}
-                />
-            ))}
-            <div className="mt-3">
-                {currentStep > 0 && (
-                    <Button variant="secondary" onClick={handlePrev} className="me-2">
-                        Previous
-                    </Button>
-                )}
-                {currentStep < steps.length - 1 ? (
-                    <Button variant="primary" onClick={handleNext}>
-                        Next
-                    </Button>
-                ) : (
-                    <Button variant="success" type="submit">
-                        Submit
-                    </Button>
-                )}
-            </div>
-        </Form>
+        <Card className="form-background" style={{ width: '25rem', borderRadius: '0.5rem', height: containerHeight }}>
+            <Card.Body>
+                <Form onSubmit={handleSubmit}>
+                    {steps.map((stepFields, index) => {
+                        if (index === steps.length - 1) {
+                            return (
+                                <Container key={index} className={`form-section form-section-active`}>
+                                    <div className="signup-success">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24" style={{ marginBottom: '20px', color: '#4BB543' }}>
+                                            <path fill="currentColor" d="M9 19l-7-7 1.41-1.42L9 16.17l11.59-11.59L22 6l-13 13z"/>
+                                        </svg>
+                                        <h2>Signup Successful!</h2>
+                                        <p>Your account has been successfully created. You're now apart of the Prototype community!</p>
+                                        <Button variant="success" onClick={() => {/* Navigate to dashboard or login page */}}>Proceed to Home Page</Button>
+                                    </div>
+                                </Container>
+                            );
+                        }
+                        return (
+                            <Container
+                                ref={activeStepRef}
+                                key={index}
+                                className={`form-section ${currentStep === index ? 'form-section-active' : ''}`}
+                            >
+                                {stepFields.map(field => (
+                                    <GenericField
+                                        key={field.name}
+                                        label={field.label}
+                                        type={field.type}
+                                        name={field.name}
+                                        value={formData[field.name] || ''}
+                                        placeholder={field.placeholder}
+                                        onChange={handleChange}
+                                        isInvalid={!!errors[field.name]}
+                                        errorMessage={errors[field.name]}
+                                    />
+                                ))}
+                            </Container>
+                        );
+                    })}
+                    <Container className="btn-container">
+                        {currentStep > 0 && currentStep < steps.length - 1 && (
+                            <Button variant="secondary" onClick={handlePrev}>
+                                Previous
+                            </Button>
+                        )}
+                        {currentStep < steps.length - 1 ? (
+                            <Button variant="primary" onClick={handleNext}>
+                                Next
+                            </Button>
+                        ) : null}
+                        {currentStep === steps.length - 1 && (
+                            <Button type="submit">
+                                Submit
+                            </Button>
+                        )}
+                    </Container>
+                </Form>
+            </Card.Body>
+        </Card>
     );
 };
