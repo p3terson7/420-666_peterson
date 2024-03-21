1a68ebe991ece8dc1eeeb96b5ee1891d0855e96a
p3terson7 <psarateanu@hotmail.ca>
2024-03-20 20:25:15 -0400

    sign_in: navbar styliiish

---
+++ masterchief/masterchief-frontend/src/assets/icons/icons.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import '../../App.css'
+export const InfoBox = () => (
+    <svg className="navbar-icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M3 3h2v18H3V3zm16 0H5v2h14v14H5v2h16V3h-2zm-8 6h2V7h-2v2zm2 8h-2v-6h2v6z" fill="currentColor"/>
+    </svg>
+);
+
+export const HomeIcon = () => (
+    <svg className="navbar-icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M14 2h-4v2H8v2H6v2H4v2H2v2h2v10h7v-6h2v6h7V12h2v-2h-2V8h-2V6h-2V4h-2V2zm0 2v2h2v2h2v2h2v2h-2v8h-3v-6H9v6H6v-8H4v-2h2V8h2V6h2V4h4z" fill="currentColor"/>
+    </svg>
+);
+
+export const UserIcon = () => (
+    <svg className="navbar-icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M2 3H0v18h24V3H2zm20 2v14H2V5h20zM10 7H6v4h4V7zm-6 6h8v4H4v-4zm16-6h-6v2h6V7zm-6 4h6v2h-6v-2zm6 4h-6v2h6v-2z" fill="currentColor"/>
+    </svg>
+);
+
+export const MessageIcon = () => (
+    <svg className="navbar-icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M4 2h18v16H6v2H4v-2h2v-2h14V4H4v18H2V2h2zm5 7H7v2h2V9zm2 0h2v2h-2V9zm6 0h-2v2h2V9z" fill="currentColor"/>
+    </svg>
+);
+
+export const DashboardIcon = () => (
+    <svg className="navbar-icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M3 3h8v10H3V3zm2 2v6h4V5H5zm8-2h8v6h-8V3zm2 2v2h4V5h-4zm-2 6h8v10h-8V11zm2 2v6h4v-6h-4zM3 15h8v6H3v-6zm2 2v2h4v-2H5z" fill="currentColor"/>
+    </svg>
+);
--- masterchief/masterchief-frontend/src/components/AppHeader/index.tsx
+++ masterchief/masterchief-frontend/src/components/AppHeader/index.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import './Navbar.css';
 import '../../App.css';
-import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {UserIcon, InfoBox, MessageIcon, DashboardIcon} from "../../assets/icons/icons";
+
 
 const AppHeader = () => {
     return (
         <Navbar className="navbar">
-            <Navbar.Brand className="navbar-brand">Prototype</Navbar.Brand>
-            <Nav className="navbar-menu">
-                <Button>
-                    ay
-                    <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path d="M3 3h2v18H3V3zm16 0H5v2h14v14H5v2h16V3h-2zm-8 6h2V7h-2v2zm2 8h-2v-6h2v6z" fill="currentColor"/>
-                    </svg>
-                </Button>
-                <a className="navbar-item" href="/services">Services</a>
-                <a className="navbar-item" href="/contact">Contact</a>
-            </Nav>
+            <div className="navbar-left">
+                <Container fluid className="navbar-item p-0" style={{background: 'none'}}>
+                    <DashboardIcon />
+                </Container>
+                <Navbar.Brand className="navbar-brand" style={{ fontFamily: "RetroGaming", color: '#373c41'}}>PROTOTYPE</Navbar.Brand>
+            </div>
+            <div className="navbar-right">
+                <Nav className="navbar-menu">
+                    <Container fluid className="navbar-item" style={{background: 'none'}}>
+                        <UserIcon />
+                        <span className="icon-description">Profile</span>
+                    </Container>
+                    <Container fluid className="navbar-item" style={{background: 'none'}}>
+                        <MessageIcon />
+                        <span className="icon-description">Messages</span>
+                    </Container>
+                    <Container fluid className="navbar-item" style={{background: 'none'}}>
+                        <InfoBox />
+                        <span className="icon-description">Info</span>
+                    </Container>
+                </Nav>
+            </div>
         </Navbar>
     );
 }
