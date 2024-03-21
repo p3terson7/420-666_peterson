ea3857b2d5193ae8eab530ca985c5e2dce87aef4
p3terson7 <psarateanu@hotmail.ca>
2024-03-20 20:33:35 -0400

    sign_in: refactor

--- masterchief/masterchief-frontend/src/components/AppHeader/index.tsx
+++ masterchief/masterchief-frontend/src/components/AppHeader/index.tsx
@@ -9,22 +9,22 @@ const AppHeader = () => {
     return (
         <Navbar className="navbar">
             <div className="navbar-left">
-                <Container fluid className="navbar-item p-0" style={{background: 'none'}}>
+                <Container fluid className="navbar-item p-0">
                     <DashboardIcon />
                 </Container>
-                <Navbar.Brand className="navbar-brand" style={{ fontFamily: "RetroGaming", color: '#373c41'}}>PROTOTYPE</Navbar.Brand>
+                <Navbar.Brand className="navbar-brand">PROTOTYPE</Navbar.Brand>
             </div>
             <div className="navbar-right">
                 <Nav className="navbar-menu">
-                    <Container fluid className="navbar-item" style={{background: 'none'}}>
+                    <Container fluid className="navbar-item">
                         <UserIcon />
                         <span className="icon-description">Profile</span>
                     </Container>
-                    <Container fluid className="navbar-item" style={{background: 'none'}}>
+                    <Container fluid className="navbar-item">
                         <MessageIcon />
                         <span className="icon-description">Messages</span>
                     </Container>
-                    <Container fluid className="navbar-item" style={{background: 'none'}}>
+                    <Container fluid className="navbar-item">
                         <InfoBox />
                         <span className="icon-description">Info</span>
                     </Container>
