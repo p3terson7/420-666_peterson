81dfa2a2db4d13dff80de57db57e3191911b9029
p3terson7 <psarateanu@hotmail.ca>
2024-03-24 17:47:33 -0400

    sign_in: defined JwtManipulator util class

---
+++ masterchief/masterchief-backend/src/main/java/com/example/masterchief/utils/JwtManipulator.java
@@ -0,0 +1,72 @@
+package com.example.masterchief.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.masterchief.dto.AdminDTO;
+import com.example.masterchief.dto.ClientDTO;
+import com.example.masterchief.dto.UserDTO;
+import com.example.masterchief.security.config.Authorities;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JwtManipulator {
+    private final String JWT_PREFIX = "Bearer ";
+    private final String issuer;
+    private final Algorithm signingAlgorithm;
+    private final long expirationMs;
+
+    @Autowired
+    public JwtManipulator(
+            @Value("${jwt.issuer}") String issuer,
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expirationMs}") int expirationMs
+    ) {
+        this.issuer = issuer;
+        this.expirationMs = expirationMs;
+        this.signingAlgorithm = Algorithm.HMAC256(secret);
+    }
+
+    public List<String> determineAuthorities(UserDTO user) {
+        List<String> authorities = new ArrayList<>();
+
+        if (user instanceof AdminDTO)
+            authorities.add(Authorities.ADMIN.name());
+        if (user instanceof ClientDTO)
+            authorities.add(Authorities.CLIENT.name());
+
+        authorities.add(Authorities.USER.name());
+
+        return authorities;
+    }
+
+    public DecodedJWT decodeToken(String token) {
+        return JWT.require(signingAlgorithm)
+                .withIssuer(issuer)
+                .build()
+                .verify(token);
+    }
+
+    public Optional<String> getJwt(HttpServletRequest request) {
+        String jwt = request.getHeader("Authorization");
+        if (!isValid(jwt))
+            return Optional.empty();
+
+        return Optional.of(jwt.substring(JWT_PREFIX.length()));
+    }
+
+    private boolean isValid(String jwt) {
+        if (jwt == null || jwt.isEmpty())
+            return false;
+
+        return jwt.length() > JWT_PREFIX.length() && jwt.startsWith(JWT_PREFIX);
+    }
+}
