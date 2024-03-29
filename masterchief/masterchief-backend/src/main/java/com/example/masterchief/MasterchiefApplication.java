7160e69f743da7213ebd690eae7f73316fb58a86
p3terson7 <psarateanu@hotmail.ca>
2024-03-29 10:37:46 -0400

    sign_in: hardcoded an admin

--- masterchief/masterchief-backend/src/main/java/com/example/masterchief/MasterchiefApplication.java
+++ masterchief/masterchief-backend/src/main/java/com/example/masterchief/MasterchiefApplication.java
@@ -1,6 +1,8 @@
 package com.example.masterchief;
 
 import com.example.masterchief.dto.AdminDTO;
+import com.example.masterchief.service.AdminService;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Optional;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class MasterchiefApplication implements CommandLineRunner {
-
+	private AdminService adminService;
 	public static void main(String[] args) {
 		SpringApplication.run(MasterchiefApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Optional<AdminDTO> optionalAdminDTO = Optional.empty();
+		Optional<AdminDTO> optionalAdminDTO = adminService.createAdmin(new AdminDTO(null, "Karim", "Benzema", "kb9@email.com", "Lolface1232!"));
+		if (optionalAdminDTO.isPresent()) {
+			System.out.println("Admin created successfully");
+		} else {
+			System.out.println("Admin creation failed");
+		}
 	}
 }
