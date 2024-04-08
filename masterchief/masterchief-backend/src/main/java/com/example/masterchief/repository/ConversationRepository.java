69fe1e7f4cd4a804cf5a2e5683c95d46d57223d3
p3terson7 <psarateanu@hotmail.ca>
2024-04-08 19:58:24 -0400

    messaging: find every conversation for a user query repository method

--- masterchief/masterchief-backend/src/main/java/com/example/masterchief/repository/ConversationRepository.java
+++ masterchief/masterchief-backend/src/main/java/com/example/masterchief/repository/ConversationRepository.java
@@ -3,10 +3,19 @@ package com.example.masterchief.repository;
 import com.example.masterchief.model.Admin;
 import com.example.masterchief.model.Client;
 import com.example.masterchief.model.Conversation;
+import com.example.masterchief.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ConversationRepository extends JpaRepository<Conversation, Long>{
     Optional<Conversation> findByAdminAndClient(Admin admin, Client client);
+
+    Optional<List<Conversation>> findByAdmin(Admin admin);
+    Optional<List<Conversation>> findByClient(Client client);
+
+    @Query("SELECT c FROM Conversation c WHERE c.admin.id = ?1 OR c.client.id = ?1")
+    List<Conversation> findAllByAdminOrClientId(Long userId);
 }
